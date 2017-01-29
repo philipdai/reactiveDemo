3d946d07ef8534c6f972e8cc29869829fb7e3387
Philip Dai <smartielion@gmail.com>
2017-01-29 01:06:38 -0500

    String stream observables sample

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
-alert(2);
+import $ from 'jquery';
+import Rx from 'rxjs/Rx';
+
+const input = $('#input');
+const button = $('#button');
+const coords = $('#coords');
+
+const buttonStream$ = Rx.Observable.fromEvent(button, 'click');
+
+buttonStream$.subscribe((x) => {
+  console.log('Clicked!');
+}, (err) => {
+  console.log(err);
+}, () => {
+  console.log('Completed!');
+});
+
+const inputStream$ = Rx.Observable.fromEvent(input, 'keyup');
+
+inputStream$.subscribe(x => console.log(x.target.value));
+
+const mouseMove$ = Rx.Observable.fromEvent(document, 'mousemove')
+  .subscribe((x) => {
+    // console.log(x.clientX, x.clientY);
+    coords.html('X: ' + x.clientX + ' Y: ' + x.clientY);
+  });
+
